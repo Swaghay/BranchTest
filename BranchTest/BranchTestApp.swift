20a308adf6256372b32ceec9441d37b60f911ed7
Abhay Upadhyaya <64004046@ep-student.org>
2020-11-23 10:42:35 -0600

    Initial Commit

---
+++ BranchTest/BranchTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BranchTestApp.swift
+//  BranchTest
+//
+//  Created by 64004046 on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct BranchTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
